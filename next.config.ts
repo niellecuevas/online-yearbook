18f35c2288bff38ac207741c711c095dbef36c8e
niellecuevas <raniella810@gmail.com>
2025-07-21 22:20:52 +0800

    vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export',
   images: {
-    unoptimized: true, 
+    // unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
