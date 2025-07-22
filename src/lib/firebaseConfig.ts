5d0eaa9b70a9996f34006457e4b6adb66ac463ae
enzouro <lorenzmayo15@gmail.com>
2025-07-22 23:35:58 +0800

    .env configs

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCYFEwwjD0I8Hk52X-dhqeWRCxATPEBlJg",
-  authDomain: "online-yearbook-d430b.firebaseapp.com",
-  projectId: "online-yearbook-d430b",
-  storageBucket: "online-yearbook-d430b.firebasestorage.app",
-  messagingSenderId: "55777153724",
-  appId: "1:55777153724:web:e84d0631ab9b2816c336b9"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export {db};
+export { db };
